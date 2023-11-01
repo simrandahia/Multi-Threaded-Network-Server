b6e56e0a87ac189d9bb40a2ce8d7786d813393fc
Linhngo268 <cunlinh268@gmail.com>
2023-11-01 16:55:25 +1030

    1

--- assignment3.py
+++ assignment3.py
@@ -3,7 +3,7 @@ import argparse
 import selectors
 import socket
 
-COUNT = 1
+COUNT = 0
 
 class Node:
     def __init__(self, data):
@@ -11,19 +11,47 @@ class Node:
         self.next = None
         self.book_next=None
 
+class Book:
+    def __init__(self, name):
+        self.name = name
+        self.content_head=None
+        self.content_tail=None
+    def add_line(self, line):
+        new_node = Node(line)
+        if self.content_head is None:
+            self.content_head = new_node
+            self.content_tail=new_node
+        else:
+            self.content_tail.next=new_node
+            self.content_tail=new_node
+
+    def display_content(self):
+        current=self.content_head
+        while current:
+            print(f"",self.name,current.data)
+            current=current.next
+
+    def save_to_file(self):
+        with open(f"{self.name}.txt", "w") as file:
+            current = self.content_head
+            while current:
+                file.write(current.data)
+                current = current.next
+        print(f"{self.name} saved on server")
+
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.shared_head = None
 
     def appendNode(self, data):
         new_node = Node(data)
-        if not self.head:
-            self.head = new_node
+        if self.shared_head is None:
+            self.shared_head = new_node
         else:
-            last_node = self.head
-            while last_node.next:
-                last_node = last_node.next
-            last_node.next = new_node
+            current = self.shared_head
+            while current.next:
+                current = current.next
+            current.next = new_node
 
     def print_list(self):
         elements = []
@@ -34,44 +62,20 @@ class LinkedList:
         print(" -> ".join(elements))
 
     def getlast(self):
-        last_node = self.head
-        while last_node:
-            last_node = last_node.next
-        print("->", last_node.data.name)
-
-class Book:
-    def __init__(self, name):
-        self.name = name
-        self.content_head=None
-        self.content_tail=None
-
-    def add_line(self, line):
-        new_node = Node(line)
-        if self.content_head is None:
-            self.content_head = new_node
-            self.content_tail = new_node
-        else:
-            self.content_tail.next = new_node
-            self.content_tail = new_node
-
-    def display_content(self):
-        current=self.content_head
+        current = self.shared_head
         while current:
-            print(f"",self.name,current.data)
-            current=current.next
+            print(f"Received Book: {current.data.name}")
+            current = current.next
 
-    def save_to_file(self):
-        with open(f"{self.name}.txt", "w") as file:
-            file.writelines(self.content)
-        print(f"{self.name} saved on server")
+
 
 class EchoNIOServer:
     def __init__(self, address, port):
         self.linked_list = LinkedList()
         self.selector = selectors.DefaultSelector()
         self.listen_address = (address, port)
-        self.first_line_received = False
         self.books = []
+        self.previous_clientaddress=[]
 
     def start_server(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -95,7 +99,8 @@ class EchoNIOServer:
         try:
             client_socket, client_address = server_socket.accept()
             print(f"Connected to: {client_address}")
-            self.first_line_received = True
+            print(f"",client_address[1])
+            self.previous_clientaddress.append(client_address[1])
             client_socket.setblocking(False)
             self.selector.register(client_socket, selectors.EVENT_READ, data=b'')
         except socket.error as e:      
@@ -106,38 +111,36 @@ class EchoNIOServer:
         if mask & selectors.EVENT_READ:
             data = sock.recv(1024)
             if data:
-                # print(f"Got: {data.decode()}")
-                # book_name = self.determine_book_name(data.decode())  # Replace with logic to identify book name
-                # book = self.get_or_create_book(book_name)
-                # book.add_line(data.decode())
                 try:
                     decoded_data = data.decode('utf-8')
                     # print(f"Got: {decoded_data}")
                     book_name = self.determine_book_name(decoded_data)
                     book = self.get_or_create_book(book_name)
                     book.add_line(decoded_data)
-                    book.display_content()
+                    self.linked_list.appendNode(book)
+                    self.linked_list.getlast()
 
                     # Save data to a file
                     with open(f"{book_name}.txt", "a") as file:
                         file.write(decoded_data)
-
                 except UnicodeDecodeError as e:
-                    # Handle the decoding error gracefully
                     print(f"Error decoding data: {e}")
-
             else:
                 print(f"Connection closed by client: {sock.getpeername()}")
                 self.selector.unregister(sock)
                 sock.close()
 
     def determine_book_name(self, data):
-        # Implement logic to identify the book name from data
         global COUNT
-        # COUNT += 1
-        print("Hello1")
-        return f"book_0{COUNT}"  
-
+        for i in range(len(self.previous_clientaddress)-1):
+                if self.previous_clientaddress[i]!=self.previous_clientaddress[i+1]:
+                    COUNT += 1
+                    print("newbook1")
+                    return f"book_0{COUNT}"
+                else:
+                    print("newbook")
+                    return f"book_0{COUNT}"
+        
     def get_or_create_book(self, name):
         for book in self.books:
             if book.name == name:
