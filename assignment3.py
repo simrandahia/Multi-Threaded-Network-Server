ee6b68c38c671fa34baf7de2b3eec601f289864f
PKaushik111 <thisispragyakaushik@gmail.com>
2023-11-12 17:00:55 +1030

    submission stuff

--- assignment3.py
+++ assignment3.py
@@ -1,8 +1,14 @@
-# works on 9pm 29/oct/23
+# python3 assignment3.py -l localhost -p "little"
+# nc localhost 9093 -i 1 <bookA.txt
+# nc localhost 9093 -i 1 <bookB.txt
+
 import argparse
 import selectors
 import socket
 import threading
+import time
+from collections import Counter
+
 # Reference: 
 # https://medium.com/coderscorner/tale-of-client-server-and-socket-a6ef54a74763
 # https://github.com/arukshani/JavaIOAndNIO/blob/master/src/com/ruk/blocking/io/EchoIOServer.java
@@ -10,16 +16,19 @@ import threading
 COUNT = 0
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, book=None):
         self.data = data
         self.next = None
-        self.book_next=None
-
+        self.book_next = None
+        self.next_frequent_search = None
+        self.book = book
 class Book:
     def __init__(self, name):
         self.name = name
         self.content_head=None
         self.content_tail=None
+        self.lock = threading.Lock()
+        
     def add_line(self, line):
         new_node = Node(line)
         if self.content_head is None:
@@ -32,7 +41,7 @@ class Book:
     def display_content(self):
         current=self.content_head
         while current:
-            print(f"",self.name,current.data)
+            # print(f"",self.name,current.data)
             current=current.next
 
     def save_to_file(self):
@@ -43,28 +52,79 @@ class Book:
                 current = current.next
         print(f"{self.name} saved on server")
 
+    def update_frequent_searches(self, new_node):
+        with self.lock:
+            new_node.next_frequent_search = self.content_head
+            self.content_head = new_node
+
 class LinkedList:
     def __init__(self):
         self.shared_head = None
         self.received_data = []
+        self.lock = threading.Lock()
 
     def appendNode(self, data):
-        self.received_data.append(data)
+        with self.lock:
+            self.received_data.append(data)
 
     def printBook(self):
-        for data in self.received_data:
-            print(f"Received Book: {data.name}")
+        with self.lock:
+            for data in self.received_data:
+                print(f"Received Book: {data.name}")
 
+class AnalysisThread(threading.Thread):
+    def __init__(self, linked_list, search_pattern, interval):
+        super().__init__()
+        self.linked_list = linked_list
+        self.search_pattern = search_pattern
+        self.interval = interval
+        self.stop_event = threading.Event()
 
+    def run(self):
+        while not self.stop_event.is_set():
+            time.sleep(self.interval)
+            if self.stop_event.is_set():
+                break
+            self.analyze_and_print()
 
+    def analyze_and_print(self):
+        with self.linked_list.lock:
+            current_node = self.linked_list.shared_head.next  # Skip the dummy node
+            nodes_with_pattern = []
+
+            while current_node is not None:
+                if current_node.data is not None and self.search_pattern in current_node.data:
+                    nodes_with_pattern.append(current_node)
+                current_node = current_node.next
+
+            title_frequency = Counter([node.book.name for node in nodes_with_pattern if node.book is not None])
+            sorted_titles = sorted(title_frequency.items(), key=lambda x: (x[1], x[0]), reverse=True)
+            
+            # Acquire a lock before printing
+            with threading.Lock():
+                print(f"\nBooks titles sorted by frequency of search pattern '{self.search_pattern}':")
+                for title, frequency in sorted_titles:
+                    print(f"{title}: {frequency} occurrences")
+                    
+
+def start_analysis_threads(linked_list, search_pattern, interval, num_threads):
+    threads = []
+    for _ in range(num_threads):
+        analysis_thread = AnalysisThread(linked_list, search_pattern, interval)
+        analysis_thread.start()
+        threads.append(analysis_thread)
+    return threads
 
 class Non_blocking_server:
-    def __init__(self, address, port):
+    def __init__(self, address, port, search_pattern, analysis_interval, num_analysis_threads):
         self.linked_list = LinkedList()
         self.selector = selectors.DefaultSelector()
         self.listen_address = (address, port)
         self.books = []
         self.new_book=[]
+        self.analysis_threads = start_analysis_threads(
+            self.linked_list, search_pattern, analysis_interval, num_analysis_threads
+        )
 
     def start_server(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -83,7 +143,8 @@ class Non_blocking_server:
                 if key.data is None:
                     self.accept_connection(key.fileobj)
                 else:
-                    self.service_connection(key, mask)
+                    # self.service_connection(key, mask)
+                    threading.Thread(target=self.service_connection, args=(key,)).start()
 
     def accept_connection(self, server_socket):
         try:
@@ -99,34 +160,49 @@ class Non_blocking_server:
 
     def service_connection(self, key, mask):
         sock = key.fileobj
+        book = key.data
         if mask & selectors.EVENT_READ:
             data = sock.recv(1024)
             if data:
                 try:
                     decoded_data = data.decode('utf-8')
-                    book=self.books[-1]
-                    book_name = book.name
-                    book.add_line(decoded_data)
-                    book.display_content()
-                    self.linked_list.appendNode(book)
-                    self.linked_list.printBook()
-                    with open(f"{book_name}.txt", "a") as file:
-                        file.write(decoded_data)
+                    with book.lock:
+                        book.add_line(decoded_data)
+                        book.display_content()
+                        with open(f"{book.name}.txt", "a") as file:
+                            file.write(decoded_data)
+                        new_node = Node(decoded_data, book)
+                        new_node.book_next = book.content_head
+                        with self.linked_list.lock:
+                            new_node.next = self.linked_list.shared_head
+                            self.linked_list.shared_head = new_node
+                            new_node.next_frequent_search = self.linked_list.shared_head
+                            self.linked_list.shared_head = new_node
+                            print(f"Added node to the shared list ({book.name}): {decoded_data}")
                 except UnicodeDecodeError as e:
                     print(f"Error decoding data: {e}")
             else:
                 print(f"Connection closed by client: {sock.getpeername()}")
                 self.selector.unregister(sock)
                 sock.close()
+                self.linked_list.appendNode(book)
+                self.linked_list.printBook()
+                book.save_to_file()
 
-     
+    def stop_analysis_threads(self):
+        for thread in self.analysis_threads:
+            thread.stop_event.set()
+            thread.join()
     
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description="Echo Server")
     parser.add_argument('-l', '--listen', type=int, default=9093, help='Port number to listen on')
     parser.add_argument('-p', '--param', type=str, default="happy", help='Parameter -p')
+    parser.add_argument('-i', '--interval', type=int, default=5, help='Analysis interval in seconds')
+    parser.add_argument('-t', '--num-threads', type=int, default=2, help='Number of analysis threads')
     args = parser.parse_args()
 
     server = Non_blocking_server('localhost', args.listen)  # Use the specified port
     server.start_server()
+    server.stop_analysis_threads()
