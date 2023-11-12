8e7696093a89e04c92870004ce819c715d2d24a4
PKaushik111 <thisispragyakaushik@gmail.com>
2023-11-12 17:59:55 +1030

    submission stuff

--- record6.py
+++ record6.py
@@ -1,4 +1,5 @@
-# works at 6am 12-nov-23 
+# Fidgeting at 6pm 12-nov-23
+
 # python3 assignment3.py -l localhost -p "little"
 # nc localhost 9093 -i 1 <bookA.txt
 # nc localhost 9093 -i 1 <bookB.txt
@@ -9,7 +10,12 @@ import socket
 import threading
 import time
 from collections import Counter
-import logging
+
+# Reference: 
+# https://medium.com/coderscorner/tale-of-client-server-and-socket-a6ef54a74763
+# https://github.com/arukshani/JavaIOAndNIO/blob/master/src/com/ruk/blocking/io/EchoIOServer.java
+
+COUNT = 0
 
 class Node:
     def __init__(self, data, book=None):
@@ -18,28 +24,28 @@ class Node:
         self.book_next = None
         self.next_frequent_search = None
         self.book = book
-
 class Book:
-    def __init__(self, name):
+    def __init__(self, name, port):
         self.name = name
+        self.port = port
         self.content_head = None
         self.content_tail = None
         self.lock = threading.Lock()
-
+        
     def add_line(self, line):
         new_node = Node(line)
         if self.content_head is None:
             self.content_head = new_node
-            self.content_tail = new_node
+            self.content_tail=new_node
         else:
-            self.content_tail.next = new_node
-            self.content_tail = new_node
+            self.content_tail.next=new_node
+            self.content_tail=new_node
 
     def display_content(self):
-        current = self.content_head
+        current=self.content_head
         while current:
-            # print(f"{self.name}: {current.data}")
-            current = current.next
+            # print(f"",self.name,current.data)
+            current=current.next
 
     def save_to_file(self):
         with open(f"{self.name}.txt", "w") as file:
@@ -48,7 +54,7 @@ class Book:
                 file.write(current.data)
                 current = current.next
         print(f"{self.name} saved on server")
-        
+
     def update_frequent_searches(self, new_node):
         with self.lock:
             new_node.next_frequent_search = self.content_head
@@ -56,7 +62,7 @@ class Book:
 
 class LinkedList:
     def __init__(self):
-        self.shared_head = Node(None)
+        self.shared_head = None
         self.received_data = []
         self.lock = threading.Lock()
 
@@ -112,15 +118,16 @@ def start_analysis_threads(linked_list, search_pattern, interval, num_threads):
         threads.append(analysis_thread)
     return threads
 
-class NonBlockingServer:
-    def __init__(self, address, port, search_pattern, analysis_interval, num_analysis_threads):
+class Non_blocking_server:
+    def __init__(self, address, initial_port, search_pattern, analysis_interval, num_analysis_threads):
         self.linked_list = LinkedList()
+        self.selector = selectors.DefaultSelector()
+        self.listen_address = (address, initial_port)
+        self.books = []
+        self.new_book = []
         self.analysis_threads = start_analysis_threads(
             self.linked_list, search_pattern, analysis_interval, num_analysis_threads
         )
-        self.selector = selectors.DefaultSelector()
-        self.listen_address = (address, port)
-        self.books = []
 
     def start_server(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -129,7 +136,12 @@ class NonBlockingServer:
         server_socket.listen(5)
         server_socket.setblocking(False)
         self.selector.register(server_socket, selectors.EVENT_READ, data=None)
-        print(f"Server started on port {self.listen_address[1]}")
+
+        # Increment port for the next book
+        self.listen_address = (self.listen_address[0], self.listen_address[1] + 1)
+
+        print(f"Server started on port >> {self.listen_address[1]}")
+        print(f"addressName:{self.listen_address[0]}")
 
         while True:
             events = self.selector.select(timeout=None)
@@ -144,43 +156,48 @@ class NonBlockingServer:
             client_socket, client_address = server_socket.accept()
             print(f"Connected to: {client_address}")
             client_socket.setblocking(False)
-            new_book = Book(f"book_0{len(self.books) + 1}")
-            self.books.append(new_book)
-            self.selector.register(client_socket, selectors.EVENT_READ, data=new_book)
-            threading.Thread(target=self.service_connection, args=(client_socket,)).start()
+            self.selector.register(client_socket, selectors.EVENT_READ, data=b'')
+
+            # Assign a unique port to each book
+            new_book_port = self.listen_address[1]
+            new_book_name = f"book_{len(self.books) + 1:02d}"
+            self.new_book = Book(new_book_name, new_book_port)
+            self.books.append(self.new_book)
+
+            threading.Thread(target=self.service_connection, args=(client_socket, self.new_book)).start()
         except socket.error as e:
             print(f"Socket error: {e}")
 
-    def service_connection(self, key):
+    def service_connection(self, key, mask):
         sock = key.fileobj
         book = key.data
-
-        data = sock.recv(1024)
-        if data:
-            try:
-                decoded_data = data.decode('utf-8')
-                with book.lock:
-                    book.add_line(decoded_data)
-                    book.display_content()
-                    with open(f"{book.name}.txt", "a") as file:
-                        file.write(decoded_data)
-                    new_node = Node(decoded_data, book)
-                    new_node.book_next = book.content_head
-                    with self.linked_list.lock:
-                        new_node.next = self.linked_list.shared_head
-                        self.linked_list.shared_head = new_node
-                        new_node.next_frequent_search = self.linked_list.shared_head
-                        self.linked_list.shared_head = new_node
-                        print(f"Added node to the shared list ({book.name}): {decoded_data}")
-            except UnicodeDecodeError as e:
-                print(f"Error decoding data: {e}")
-        else:
-            print(f"Connection closed by client: {sock.getpeername()}")
-            self.selector.unregister(sock)
-            sock.close()
-            self.linked_list.appendNode(book)
-            self.linked_list.printBook()
-            book.save_to_file()
+        if mask & selectors.EVENT_READ:
+            data = sock.recv(1024)
+            if data:
+                try:
+                    decoded_data = data.decode('utf-8')
+                    with book.lock:
+                        book.add_line(decoded_data)
+                        book.display_content()
+                        with open(f"{book.name}.txt", "a") as file:
+                            file.write(decoded_data)
+                        new_node = Node(decoded_data, book)
+                        new_node.book_next = book.content_head
+                        with self.linked_list.lock:
+                            new_node.next = self.linked_list.shared_head
+                            self.linked_list.shared_head = new_node
+                            new_node.next_frequent_search = self.linked_list.shared_head
+                            self.linked_list.shared_head = new_node
+                            print(f"Added node to the shared list ({book.name}): {decoded_data}")
+                except UnicodeDecodeError as e:
+                    print(f"Error decoding data: {e}")
+            else:
+                print(f"Connection closed by client: {sock.getpeername()}")
+                self.selector.unregister(sock)
+                sock.close()
+                self.linked_list.appendNode(book)
+                self.linked_list.printBook()
+                book.save_to_file()
 
     def stop_analysis_threads(self):
         for thread in self.analysis_threads:
@@ -188,16 +205,20 @@ class NonBlockingServer:
             thread.join()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Echo Server")
-    parser.add_argument('-l', '--listen', type=str, default='localhost', help='Address to listen on')
-    parser.add_argument('-port', '--port', type=int, default=9093, help='Port number to listen on')
-    parser.add_argument('-p', '--pattern', type=str, required=True, help='Search pattern for analysis')
-    parser.add_argument('-i', '--interval', type=int, default=5, help='Analysis interval in seconds')
-    parser.add_argument('-t', '--num-threads', type=int, default=2, help='Number of analysis threads')
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser(description="Echo Server")
+        parser.add_argument('-l', '--listen', type=str, default='localhost', help='Address to listen on')
+        parser.add_argument('-p', '--param', type=str, default="happy", help='Parameter -p')
+        parser.add_argument('-i', '--interval', type=int, default=5, help='Analysis interval in seconds')
+        parser.add_argument('-t', '--num-threads', type=int, default=2, help='Number of analysis threads')
+        args = parser.parse_args()
 
-    print(args.pattern)
+        # Pass the additional parameters to the Non_blocking_server constructor
+        server = Non_blocking_server(args.listen, int(args.listen), args.param, args.interval, args.num_threads)
+        server.start_server()
+        server.stop_analysis_threads()
 
-    server = NonBlockingServer(args.listen, args.port, args.pattern, args.interval, args.num_threads)
-    server.start_server()
-    server.stop_analysis_threads()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
