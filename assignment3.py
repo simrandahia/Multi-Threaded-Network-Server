23bd2ac7c4828ab49ee84b2998feae95afc81861
PKaushik111 <thisispragyakaushik@gmail.com>
2023-11-12 16:44:23 +1030

    network-stuff

--- assignment3.py
+++ assignment3.py
@@ -1,34 +1,39 @@
+# works on 9pm 29/oct/23
 import argparse
 import selectors
 import socket
 import threading
+# Reference: 
+# https://medium.com/coderscorner/tale-of-client-server-and-socket-a6ef54a74763
+# https://github.com/arukshani/JavaIOAndNIO/blob/master/src/com/ruk/blocking/io/EchoIOServer.java
+
+COUNT = 0
 
 class Node:
-    def _init_(self, data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+        self.book_next=None
 
 class Book:
-    def _init_(self, name):
+    def __init__(self, name):
         self.name = name
-        self.content_head = None
-        self.content_tail = None
-        self.lock = threading.Lock()  # Lock for each book
-
+        self.content_head=None
+        self.content_tail=None
     def add_line(self, line):
         new_node = Node(line)
         if self.content_head is None:
             self.content_head = new_node
-            self.content_tail = new_node
+            self.content_tail=new_node
         else:
-            self.content_tail.next = new_node
-            self.content_tail = new_node
+            self.content_tail.next=new_node
+            self.content_tail=new_node
 
     def display_content(self):
-        current = self.content_head
+        current=self.content_head
         while current:
-            print(f"{self.name}: {current.data}")
-            current = current.next
+            print(f"",self.name,current.data)
+            current=current.next
 
     def save_to_file(self):
         with open(f"{self.name}.txt", "w") as file:
@@ -39,7 +44,7 @@ class Book:
         print(f"{self.name} saved on server")
 
 class LinkedList:
-    def _init_(self):
+    def __init__(self):
         self.shared_head = None
         self.received_data = []
 
@@ -50,12 +55,16 @@ class LinkedList:
         for data in self.received_data:
             print(f"Received Book: {data.name}")
 
-class NonBlockingServer:
-    def _init_(self, address, port):
+
+
+
+class Non_blocking_server:
+    def __init__(self, address, port):
         self.linked_list = LinkedList()
         self.selector = selectors.DefaultSelector()
         self.listen_address = (address, port)
         self.books = []
+        self.new_book=[]
 
     def start_server(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -64,7 +73,9 @@ class NonBlockingServer:
         server_socket.listen(5)
         server_socket.setblocking(False)
         self.selector.register(server_socket, selectors.EVENT_READ, data=None)
-        print(f"Server started on port {self.listen_address[1]}")
+        #to determine the netcat network 
+        print(f"Server started on port >> {self.listen_address[1]}")
+        print(f"addressName:{self.listen_address[0]}")
 
         while True:
             events = self.selector.select(timeout=None)
@@ -72,49 +83,50 @@ class NonBlockingServer:
                 if key.data is None:
                     self.accept_connection(key.fileobj)
                 else:
-                    threading.Thread(target=self.service_connection, args=(key,)).start()
+                    self.service_connection(key, mask)
 
     def accept_connection(self, server_socket):
         try:
             client_socket, client_address = server_socket.accept()
             print(f"Connected to: {client_address}")
             client_socket.setblocking(False)
-            new_book = Book(f"book_0{len(self.books) + 1}")
-            self.books.append(new_book)
-            self.selector.register(client_socket, selectors.EVENT_READ, data=new_book)
-            threading.Thread(target=self.service_connection, args=(client_socket,)).start()
-        except socket.error as e:
+            self.selector.register(client_socket, selectors.EVENT_READ, data=b'')
+            self.new_book = Book(f"book_0{len(self.books) + 1}")
+            self.books.append(self.new_book)
+            threading.Thread(target=self.service_connection, args=(client_socket, self.new_book)).start()
+        except socket.error as e:      
             print(f"Socket error: {e}")
 
-    def service_connection(self, key):
+    def service_connection(self, key, mask):
         sock = key.fileobj
-        book = key.data
-
-        data = sock.recv(1024)
-        if data:
-            try:
-                decoded_data = data.decode('utf-8')
-                with book.lock:
+        if mask & selectors.EVENT_READ:
+            data = sock.recv(1024)
+            if data:
+                try:
+                    decoded_data = data.decode('utf-8')
+                    book=self.books[-1]
+                    book_name = book.name
                     book.add_line(decoded_data)
                     book.display_content()
-                    with open(f"{book.name}.txt", "a") as file:
+                    self.linked_list.appendNode(book)
+                    self.linked_list.printBook()
+                    with open(f"{book_name}.txt", "a") as file:
                         file.write(decoded_data)
-            except UnicodeDecodeError as e:
-                print(f"Error decoding data: {e}")
-        else:
-            print(f"Connection closed by client: {sock.getpeername()}")
-            self.selector.unregister(sock)
-            sock.close()
-            self.linked_list.appendNode(book)
-            self.linked_list.printBook()
-            book.save_to_file()
+                except UnicodeDecodeError as e:
+                    print(f"Error decoding data: {e}")
+            else:
+                print(f"Connection closed by client: {sock.getpeername()}")
+                self.selector.unregister(sock)
+                sock.close()
 
-if _name_ == '_main_':
+     
+    
+if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(description="Echo Server")
     parser.add_argument('-l', '--listen', type=int, default=9093, help='Port number to listen on')
-    parser.add_argument('-p', '--param', type=str, default='happy', help='Parameter -p')
-    parser.add_argument('-port', '--port', type=int, default=9093, help='Port number to listen on')
+    parser.add_argument('-p', '--param', type=str, default="happy", help='Parameter -p')
     args = parser.parse_args()
 
-    server = NonBlockingServer('localhost', args.listen)
+    server = Non_blocking_server('localhost', args.listen)  # Use the specified port
     server.start_server()
